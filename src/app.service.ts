b2953b62f585d2568447480b8e8e1ba15b57b032
Nayan Gupta <nayan.gupta@epsilon.com>
2025-05-14 14:19:53 +0530

    password expose

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,7 @@ export class AppService {
   getHello(param: string): string {
     const password = 'abc*123'
     console.log(param);
-    console.log('scan security........', password);
+    console.log('scan security......', password);
     return 'Hello World!';
   }
 }
