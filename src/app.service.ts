047b6577ce88b35304205620f258c638337fbd88
Nayan Gupta <nayan.gupta@epsilon.com>
2025-05-14 14:08:44 +0530

    password expose

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,8 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(param: string): string {
+    const password = 'abc*123'
     console.log(param);
-    console.log('scan security', param);
+    console.log('scan security........', password);
     return 'Hello World!';
   }
 }
